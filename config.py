99191afd8747f750435977b29836bb3dd74eb7bf
Dev BPAD NTT <infosamsatntt@gmail.com>
2021-08-10 15:46:45 +0800

    initial commit

---
+++ config.py
@@ -0,0 +1,6 @@
+consumer_key = "92KgaOiaiiasUPYWkyTw2c1fi"
+consumer_secret = "W2UlpQ0dZLqMPDuN3lsIyfgGozSPb63sFSPEeR8mjSu550hyzs"
+access_token = "1422475634505240577-NTXjRU1hsSMKNynEkLCsqQkAInl6DY"
+access_token_secret = "jDeMDKON5gCfZg9N0OYxEDI6INdG949Y2Hje0OwE8HHpV"
+trigger = "itcek"
+timezone = 7 #change this into your timezone. for example if your timezone is GMT+7 you put 7
